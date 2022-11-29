1b2b81ebd75bae33430e64f5327b0d252c9d9d72
Tyler <tylhuang@umich.edu>
2022-11-29 13:09:10 -0500

    push

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,21 @@
 let cart = {}
 
+const items = {
+    "rib": ["Short Rib Burger", 5.99],
+    "triple": ["Triple Patty Burger", 7.99],
+    "family": ["Family Meal", 9.99],
+    "burger": ["BK Burger", 3.49],
+    "waffle": ["Waffle Burger", 2.99],
+    "turkey": ["Turkey Burger", 3.49],
+    "hotdog": ["Hot Dog", 0.99],
+    "fries": ["Fries", 1.99],
+    "rings": ["Onion Rings", 2.49], 
+    "drink": ["Soda", 1.49],
+    "icecream": ["Soft Serve", 1.89],
+    "shake": ["Milk Shake", 5.99],
+    "cookie": ["Cookies", 1.99]
+}
+
 function increment(id, inc) {
     var temp = document.getElementById(id); 
     if (inc === "minus") {
@@ -21,5 +37,12 @@ function increment(id, inc) {
 }
 
 function viewCart() {
-    
+    var msg = ""; 
+    var total = 0; 
+    Object.keys(cart).forEach(key => {
+        total += (cart[key] * items[key][1]);
+        msg += items[key][0] + " (" + cart[key] + "): $" + (cart[key] * items[key][1]) + "\n";
+    });
+    msg += "Total: $" + total;
+    alert(msg);
 }
