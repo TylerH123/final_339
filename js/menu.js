f1c926d4fee6335d054f247b65f7b42112dd07de
Tyler <tylhuang@umich.edu>
2022-11-29 12:57:22 -0500

    push

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,25 @@
+let cart = {}
+
 function increment(id, inc) {
     var temp = document.getElementById(id); 
     if (inc === "minus") {
-        if (Number(temp.value) > 0) {
-            temp.value = Number(temp.value) - 1;
+        if (Number(temp.innerHTML) > 0) {
+            temp.innerHTML = Number(temp.innerHTML) - 1;
+            cart[id] -= 1; 
         }
     }
     else {
-        temp.value = Number(temp.value) + 1;
+        temp.innerHTML = Number(temp.innerHTML) + 1;
+        if (id in cart) {
+            cart[id] += 1;
+        } 
+        else {
+            cart[id] = 1;
+        }
     }
+    console.log(cart);
+}
+
+function viewCart() {
+    
 }
