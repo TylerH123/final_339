9ee2f6a2a26e61eda0776a4d024350866d84c621
Tyler <tylhuang@umich.edu>
2022-11-29 23:05:25 -0500

    push

--- js/menu.js
+++ js/menu.js
@@ -9,26 +9,26 @@ const items = {
     "turkey": ["Turkey Burger", 3.49],
     "hotdog": ["Hot Dog", 0.99],
     "fries": ["Fries", 1.99],
-    "rings": ["Onion Rings", 2.49], 
+    "rings": ["Onion Rings", 2.49],
     "drink": ["Soda", 1.49],
     "icecream": ["Soft Serve", 1.89],
     "shake": ["Milk Shake", 5.99],
-    "cookie": ["Cookies", 1.99]
+    "cookies": ["Cookies", 1.99]
 }
 
 function increment(id, inc) {
-    var temp = document.getElementById(id); 
+    var temp = document.getElementById(id);
     if (inc === "minus") {
         if (Number(temp.innerHTML) > 0) {
             temp.innerHTML = Number(temp.innerHTML) - 1;
-            cart[id] -= 1; 
+            cart[id] -= 1;
         }
     }
     else {
         temp.innerHTML = Number(temp.innerHTML) + 1;
         if (id in cart) {
             cart[id] += 1;
-        } 
+        }
         else {
             cart[id] = 1;
         }
@@ -37,12 +37,13 @@ function increment(id, inc) {
 }
 
 function viewCart() {
-    var msg = ""; 
-    var total = 0; 
+    var msg = "";
+    var total = 0;
     Object.keys(cart).forEach(key => {
         total += (cart[key] * items[key][1]);
         msg += items[key][0] + " (" + cart[key] + "): $" + (cart[key] * items[key][1]) + "\n";
     });
+    total = total.toFixed(2);
     msg += "Total: $" + total;
     alert(msg);
 }
